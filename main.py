edec5816b4ea4310b8026c03de01c1a833cbcf17
Andrey Sergeev <Andrey.kursk2002@gmail.com>
2022-05-22 21:28:58 +0300

    Добавлена возможность отсортировать файлы в соответствии с зависимостями     1)Добавлен метод топологической сортировки     2)Добавлена возможность конкатенации файлов в один в соответствии     с топологически отсортированным списком

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 
+from msilib.schema import Error
 import os
 from pathlib import Path
+import re
+import codecs
+
 # Эта функция ищет и записывает в список все пути до файлов .txt, которые
 # находятся с main.py на одном уровне вложенности и ниже
 def findAllFiles() : 
@@ -25,12 +29,68 @@ def findAllFiles() :
 txtFiles = findAllFiles()
 # Сортируем эти файлы по названию
 txtFiles.sort(key = lambda e : Path(e).name)
-
+print("Список отсортированных по названию файлов: \n" + str(txtFiles))
 # Записываем содержимое этих файлов в новый файл SortedByNameTxt.txt,
 # который будет находится рядом с main.py 
-newFile = open('SortedByNameTxt.txt', 'w+')
+newFile = codecs.open('SortedByNameTxt.txt', mode = 'w+', encoding = 'utf-8', errors = 'ignore')
 for p in txtFiles :
-    thr = open(p, 'r')
+    thr = codecs.open(p, mode = 'r', encoding = 'utf-8', errors = 'ignore')
     newFile.write(thr.read())
     thr.close()
 newFile.close()
+
+#Составим словарь с зависимостями для каждого файла. Ключ - путь до конкретного текстового файла.
+#Значение - список с путями до файлов, от которых он зависит
+D = dict()
+for p in txtFiles :
+    thr = codecs.open(p, mode = 'r', encoding = 'utf-8', errors = 'ignore')
+    data = thr.read()
+    thr.close()
+
+    #Ищем все совпадения с RegEx для нахождения всех require
+    result = re.findall("require [‘'][:\-/\\ \wА-Яа-я]*[’']", data)
+    siblings = []
+    #Получаем из require путь до файла с зависимостью и преобразуем
+    #этот путь в абсолютный
+    for elem in result :
+        elem = re.split("[‘']", elem)[1][:-1]
+        elem = os.path.join(os.getcwd(), Path(elem + '.txt'))
+        siblings.append(elem)
+    D[p] = siblings
+
+#Реализация топологической сортировки
+def TopologicalSort(G):
+    in_degrees = dict((u, 0) for u in G)
+    for u in G:
+        for v in G[u]:
+            in_degrees[v] += 1
+    queue = [u for u in G if in_degrees[u] == 0]
+    res = []
+    while queue:
+        u = queue.pop()
+        res.append(u)
+        for v in G[u]:
+            in_degrees[v] -= 1
+            
+            if in_degrees[v] == 0:
+                queue.append(v)
+    return res[::-1]
+
+
+try :
+    #Применяем топологическую сортировку к словарю с зависимостями
+    topologicalySorted = TopologicalSort(D)
+    print("Топологически отсортированный список с зависимостями: \n" + str(topologicalySorted))
+    #Объединяем все зависимые файлы в один новый и помещаем
+    #его на одном уровне вложенности с main.py
+    newFile = codecs.open('RelatedTxtFiles.txt', mode = 'w+', encoding = 'utf-8', errors = 'ignore')
+    for p in topologicalySorted :
+        thr = codecs.open(p, mode = 'r', encoding = 'utf-8', errors = 'ignore')
+        newFile.write(thr.read())
+        thr.close()
+    newFile.close()
+except KeyError :
+    #KeyError во время выполнения топологической сортировки означает наличие
+    #циклической зависимости
+    print('В этой файловой системе присутствует циклическая зависимость')
+
